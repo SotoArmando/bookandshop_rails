46556a5e32c7d95f819fd7c2cdbdd5e2b6bfeeef
Sotoarmando <armando29@live.com>
2021-09-24 13:48:03 -0400

    main: set access control all origins, testing purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    after_filter  :set_access_control_headers
+    
+    def set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+    end 
 end
