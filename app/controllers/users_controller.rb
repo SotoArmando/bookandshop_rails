3dd10ece1511c023fc839d53b256b565e7ae34da
Sotoarmando <armando29@live.com>
2021-09-25 13:28:09 -0400

    Approach mailer: views and setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,10 @@ class UsersController < ApplicationController
           # Tell the UserMailer to send a welcome email after save
           UserMailer.with(user: @user).welcome_email.deliver_now
   
+          format.html { redirect_to(@user, notice: 'User was successfully created.') }
           format.json { render json: @user, status: :created, location: @user }
         else
+          format.html { render action: 'new' }
           format.json { render json: @user.errors, status: :unprocessable_entity }
         end
       end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'dev.armando29@gmail.com',
+    password:             'vugtpezsxuzqxumu',
+    authentication:       'plain',
+    enable_starttls_auto: true ,
+  }
+  
 end
+
