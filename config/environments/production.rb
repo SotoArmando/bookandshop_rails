5b959b97d52875169df417e23f735a7200548601
Sotoarmando <armando29@live.com>
2021-09-25 13:28:42 -0400

    Approach mailer: views and setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'dev.armando29@gmail.com',
+    password:             'vugtpezsxuzqxumu',
+    authentication:       'plain',
+    enable_starttls_auto: true ,
+  }
+  
 end
