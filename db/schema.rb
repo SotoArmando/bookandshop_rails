090ebe4520ae83b9dfcd8123c207f0681f55a2f6
Sotoarmando <armando29@live.com>
2021-10-06 18:12:03 -0500

    Approach to follow design guidelines

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_203953) do
+ActiveRecord::Schema.define(version: 2021_10_06_223146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_09_30_203953) do
     t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.string "backcolor"
   end
 
   create_table "pictures", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-items = Item.create([{make:"2715",model:"B-Bone",year:2015},
-{make:"2718",model:"Besbi",year:2015},
-{make:"2726",model:"Daystar",year:2015},
-{make:"2728",model:"Daystar 125",year:2015},
-{make:"2745",model:"RoadSport",year:2015},
-{make:"2747",model:"RoadSport 125",year:2015},
-{make:"2756",model:"S1",year:2015},
-{make:"2763",model:"S3 125",year:2015},
-{make:"2764",model:"S3 Advance",year:2015},
-{make:"2773",model:"VJF 250",year:2015}])
+items = Item.create([ {make: "2715", model: "GTS Super 300 Euro 5", year: 2021,
+        description: "The Vespa GTS Super 300 with its 300 HPE (High Performance Engine) as standard is the fastest, most powerful Vespa of all time.", backcolor: "EEDD7F"},
+     {make: "2715", model: "GTS Super 300 Sport Euro 5", year: 2021,
+        description: "The totally steel bodywork with its perfect balance delivers personality and character in abundance.", backcolor: "7FB3D6"},
+     {make: "2715", model: "GTS Super 300 Racing Sixties Euro 5", year: 2021, 
+        description: "Carefully-selected materials, colours and textures, all combine to create a sporty-looking bike.", backcolor: "9E9F9F"},
+     {make: "2715", model: "GTS Super 300 Tech Euro 5", year: 2021, 
+        description: "A special matt black finish on the wheel rims that make the Vespa SuperTech such a standout.", backcolor: "979696"},
+     {make: "2715", model: "GTS 300 Euro 5", year: 2021, 
+        description: "The Vespa GTS 300 with electronic injection delivers up to 23.8 Hp and its quality construction makes for remarkable acceleration.", backcolor: "E9A997"},
+     {make: "2715", model: "GTS 300 Touring Euro 5", year: 2021,
+        description: "The Vespa GTS 300 with electronic injection delivers up to 23.8 Hp and its quality construction makes for remarkable acceleration.", backcolor: "D6CEA5"},
+     {make: "2715", model: "GTS 125 Touring Euro 5", year: 2021, 
+        description: "The 125 i-get engine with Regulator Inverter Start & Stop System is ideal for daily running.", backcolor: "E6DDBF"},
+     {make: "2715", model: "GTS 125 Euro 5", year: 2021, 
+        description: "The 125 i-get engine with Regulator Inverter Start & Stop System is ideal for daily running. ", backcolor: "D3DDD8"},
+     {make: "2715", model: "GTS 125 75th Euro 5", year: 2021, 
+        description: "The Vespa 75th sports all sorts of details that make it a true classic.", backcolor: "BEC0C2"},
+    {make: "2715", model: "GTS 300 75th Euro 5", year: 2021, 
+        description: "The Vespa 75th sports all sorts of details that make it a true classic.", backcolor: "B5B7BA"}])
+
+
 
 pictures = Picture.create([
-    {pictureid:"5562",item_id:1},
-    {pictureid:"5568",item_id:2},
-    {pictureid:"5582",item_id:3},
-    {pictureid:"5586",item_id:4},
-    {pictureid:"5610",item_id:5},
-    {pictureid:"5613",item_id:6},
-    {pictureid:"5634",item_id:7},
-    {pictureid:"5645",item_id:8},
-    {pictureid:"5647",item_id:9},
-    {pictureid:"5660",item_id:10},
+    {pictureid:"1",item_id:1},
+    {pictureid:"2",item_id:2},
+    {pictureid:"3",item_id:3},
+    {pictureid:"4",item_id:4},
+    {pictureid:"5",item_id:5},
+    {pictureid:"6",item_id:6},
+    {pictureid:"7",item_id:7},
+    {pictureid:"8",item_id:8},
+    {pictureid:"9",item_id:9},
+    {pictureid:"10",item_id:10},
 ])
